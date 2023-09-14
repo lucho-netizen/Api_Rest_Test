a571f4931a863b14eeefff7555f3c14a862e6a7b
Paco <114266465+lucho-netizen@users.noreply.github.com>
2023-09-14 14:54:38 -0500

    Beta 2.0.0

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const URL = "http://localhost:5215/api/Gasoductos";
+// Define la URL de la API con el ID 1
+var apiUrl = 'https://localhost:7293/api/Gasoductos/';
 
-async function getGasoducto() {
-  try {
-    const reponse = await fetch(URL);
-    const characters = await reponse.json();
-
-    console.log(characters);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-getGasoducto();
+// Realiza la solicitud GET a la URL
+fetch(apiUrl)
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Suponiendo que la respuesta es en formato JSON
+        } else {
+            throw new Error('Error en la respuesta de la API');
+        }
+    })
+    .then(data => {
+        // Maneja los datos recibidos de la API aquí
+        console.log('Datos de la API:', data);
+        // Puedes acceder a los datos y usarlos según sea necesario
+    })
+    .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
+    });
